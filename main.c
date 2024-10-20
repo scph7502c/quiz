b8eff98b8f8eae23c47bed68993147a37684f5a0
Damian Temimi <damian.temimi@gmail.com>
2024-10-20 23:18:40 +0200

    wip

--- main.c
+++ main.c
@@ -6,6 +6,11 @@ int main() {
 
   int total_points = 0;
   int number_of_questions = geography_size;
+  
+  for (int i = 0; i < number_of_questions; i++){
+    printf("%s\n", geography[i].question);
+  }
+
 
   printf("Current number of geography questions: %d\n", number_of_questions);
 
